2704bfd7331e0324288ee6eabd891c934789fcee
madCoderAI <m.ghatuari25@gmail.com>
2025-11-21 08:44:41 +0000

    first commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+# Download and load the model
+model_path = hf_hub_download(repo_id="yuRIrocks/Tourism-Package-Prediction", filename="best_xgboost_model_v1.joblib")
+model = joblib.load(model_path)
+
+# Streamlit UI for Machine Failure Prediction
+st.title("Tourism Package Prediction App")
+st.write("""
+This application predicts whether a customer will purchase the newly introduced Wellness Tourism Package before contacting them.
+Please enter the type of Customer and their interaction data below to get a prediction.
+""")
+
+# User input
+ProductType = st.selectbox("ProductType", ["Basic", "Deluxe", "King","Super Deluxe","Standard"])
+TypeofContact = st.selectbox("TypeofContact", ["Company Invited", "Self Inquiry"])
+Occupation = st.selectbox("Occupation", ["Salaried", "Free Lancer","Small Business","Large Business"])
+Designation = st.selectbox("Designation", ["Manager", "Executive","Senior Manager","AVP","VP"])
+MaritalStatus = st.selectbox("MaritalStatus", ["Single", "Divorced","Married","Unmarried"])
+Age = st.number_input("Age", min_value=18, max_value=61, value=36)
+NumberOfChildrenVisiting = st.number_input("NumberOfChildrenVisiting", min_value=0, max_value=3, value=1)
+OwnCar = st.selectbox("OwnCar (0: No, 1: Yes)", ["0", "1"])
+Passport = st.selectbox("Passport (0: No, 1: Yes)", ["0", "1"])
+CityTier = st.selectbox("CityTier", ["1", "2","3"])
+PreferredPropertyStar = st.number_input("Preferred Property Rating (0 to 5)", min_value=0, max_value=5, value=4)
+PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score (0 to 10)", min_value=0, max_value=10)
+
+
+# Assemble input into DataFrame
+input_data = pd.DataFrame([{
+    'ProductType': ProductType,
+    'TypeofContact': TypeofContact,
+    'Occupation': Occupation,
+    'Designation': Designation,
+    'MaritalStatus': MaritalStatus,
+    'Age': Age
+    'NumberOfChildrenVisiting': NumberOfChildrenVisiting,
+    'OwnCar': OwnCar,
+    'Passport': Passport,
+    'CityTier': CityTier,
+    'PreferredPropertyStar': PreferredPropertyStar,
+    'PitchSatisfactionScore': PitchSatisfactionScore
+}])
+
+
+if st.button("Predict Failure"):
+    prediction = model.predict(input_data)[0]
+    result = "Machine Failure" if prediction == 1 else "No Failure"
+    st.subheader("Prediction Result:")
+    st.success(f"The model predicts: **{result}**")
